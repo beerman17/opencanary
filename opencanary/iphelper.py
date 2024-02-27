dbe76aa82c508e5e26bc59ae9282bb360968cae1
Alexander Pivnyak <apivnyak@yandex-team.ru>
2024-02-27 18:10:56 +0300

    rework check_ip function to allow it works with ipv6 as well

--- opencanary/iphelper.py
+++ opencanary/iphelper.py
@@ -1,5 +1,6 @@
 import struct
 import socket
+import ipaddress
 
 
 def ip2int(addr):
@@ -10,26 +11,21 @@ def ip2int(addr):
     return struct.unpack("!I", socket.inet_aton(addr))[0]
 
 
-def check_ip(ip, network_range):
+def check_ip(ip: str, network_range: str) -> bool:
     """
     Test if the IP is in range
 
     Range is expected to be in CIDR notation format. If no MASK is
     given /32 is used. It return True if the IP is in the range.
     """
-
-    netItem = str(network_range).split("/")
-    rangeIP = netItem[0]
-    if len(netItem) == 2:
-        rangeMask = int(netItem[1])
-    else:
-        rangeMask = 32
-
     try:
-        ripInt = ip2int(rangeIP)
-        ipInt = ip2int(ip)
-        result = not ((ipInt ^ ripInt) & 0xFFFFFFFF << (32 - rangeMask))
+        ip = ipaddress.ip_address(ip)
+        network = ipaddress.ip_network(network_range, strict=False)
+        if isinstance(ip, ipaddress.IPv6Address) and ip.ipv4_mapped is not None:
+            ip = ipaddress.ip_address(ip.ipv4_mapped)
+        if ip in network:
+            return True
+        else:
+            return False
     except:  # noqa: E722
-        result = False
-
-    return result
+        return False
